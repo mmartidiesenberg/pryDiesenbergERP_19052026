f7db1cd3128a554b3070eda7e013ebc494fca0cf
mmartidiesenberg <mmmartidiesenberg@gmail.com>
2026-05-19 09:13:25 -0300

    .

---
+++ pryDiesenbergERP_19052026/clsConexion.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
+
+
+namespace pryDiesenbergERP_19052026
+{
+    public class clsConexion
+    {
+        private string connectionString;
+
+        public clsConexion()
+        {
+            string ruta = AppDomain.CurrentDomain.BaseDirectory + @"BaseDatos\Diesenberg.accdb";
+            connectionString = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={ruta};Persist Security Info=False;";
+        }
+
+        public OleDbConnection ObtenerConexion()
+        {
+            return new OleDbConnection(connectionString);
+        }
+
+        // Ejecutar consultas que no devuelven datos (INSERT, UPDATE, DELETE)
+        public int EjecutarComando(string sql, OleDbParameter[] parametros = null)
+        {
+            using (var conn = ObtenerConexion())
+            using (var cmd = new OleDbCommand(sql, conn))
+            {
+                if (parametros != null)
+                    cmd.Parameters.AddRange(parametros);
+
+                conn.Open();
+                return cmd.ExecuteNonQuery();
+            }
+        }
+
+        // Obtener datos como DataTable
+        public DataTable ObtenerDatos(string sql, OleDbParameter[] parametros = null)
+        {
+            using (var conn = ObtenerConexion())
+            using (var cmd = new OleDbCommand(sql, conn))
+            using (var adapter = new OleDbDataAdapter(cmd))
+            {
+                if (parametros != null)
+                    cmd.Parameters.AddRange(parametros);
+
+                conn.Open();
+                var tabla = new DataTable();
+                adapter.Fill(tabla);
+                return tabla;
+            }
+        }
+    }
+}
--- pryDiesenbergERP_19052026/frmPrincipal.Designer.cs
+++ pryDiesenbergERP_19052026/frmPrincipal.Designer.cs
@@ -28,20 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 405);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(585, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "Estado Conexión BD";
             // 
             // frmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(585, 427);
+            this.Controls.Add(this.statusStrip1);
             this.Name = "frmPrincipal";
             this.Text = "Principal ERP";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
     }
 }
 
