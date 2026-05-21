b67ec50eee3e513636430a79431d25e05455f5f4
mmartidiesenberg <mmmartidiesenberg@gmail.com>
2026-05-21 10:03:13 -0300

    parte de frmRRHH

--- pryDiesenbergERP_19052026/frmRRHH.Designer.cs
+++ pryDiesenbergERP_19052026/frmRRHH.Designer.cs
@@ -28,12 +28,206 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.gbDatos = new System.Windows.Forms.GroupBox();
+            this.gbDomicilio = new System.Windows.Forms.GroupBox();
+            this.lblDNI = new System.Windows.Forms.Label();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblDireccion = new System.Windows.Forms.Label();
+            this.lblGEO = new System.Windows.Forms.Label();
+            this.lblLocalidad = new System.Windows.Forms.Label();
+            this.lblProvincia = new System.Windows.Forms.Label();
+            this.txtDNI = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.txtGEO = new System.Windows.Forms.TextBox();
+            this.cmbLocalidad = new System.Windows.Forms.ComboBox();
+            this.cmbProvincia = new System.Windows.Forms.ComboBox();
+            this.gbDatos.SuspendLayout();
+            this.gbDomicilio.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbDatos
+            // 
+            this.gbDatos.Controls.Add(this.txtNombre);
+            this.gbDatos.Controls.Add(this.txtApellido);
+            this.gbDatos.Controls.Add(this.txtDNI);
+            this.gbDatos.Controls.Add(this.lblDNI);
+            this.gbDatos.Controls.Add(this.lblApellido);
+            this.gbDatos.Controls.Add(this.lblNombre);
+            this.gbDatos.Location = new System.Drawing.Point(21, 12);
+            this.gbDatos.Name = "gbDatos";
+            this.gbDatos.Size = new System.Drawing.Size(265, 189);
+            this.gbDatos.TabIndex = 0;
+            this.gbDatos.TabStop = false;
+            this.gbDatos.Text = "Datos Personales";
+            // 
+            // gbDomicilio
+            // 
+            this.gbDomicilio.Controls.Add(this.cmbProvincia);
+            this.gbDomicilio.Controls.Add(this.cmbLocalidad);
+            this.gbDomicilio.Controls.Add(this.txtGEO);
+            this.gbDomicilio.Controls.Add(this.txtDireccion);
+            this.gbDomicilio.Controls.Add(this.lblProvincia);
+            this.gbDomicilio.Controls.Add(this.lblLocalidad);
+            this.gbDomicilio.Controls.Add(this.lblGEO);
+            this.gbDomicilio.Controls.Add(this.lblDireccion);
+            this.gbDomicilio.Location = new System.Drawing.Point(21, 219);
+            this.gbDomicilio.Name = "gbDomicilio";
+            this.gbDomicilio.Size = new System.Drawing.Size(265, 219);
+            this.gbDomicilio.TabIndex = 1;
+            this.gbDomicilio.TabStop = false;
+            this.gbDomicilio.Text = "Domicilio";
+            // 
+            // lblDNI
+            // 
+            this.lblDNI.AutoSize = true;
+            this.lblDNI.Location = new System.Drawing.Point(11, 41);
+            this.lblDNI.Name = "lblDNI";
+            this.lblDNI.Size = new System.Drawing.Size(26, 13);
+            this.lblDNI.TabIndex = 0;
+            this.lblDNI.Text = "DNI";
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Location = new System.Drawing.Point(11, 96);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(44, 13);
+            this.lblApellido.TabIndex = 1;
+            this.lblApellido.Text = "Apellido";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(11, 145);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.TabIndex = 2;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblDireccion
+            // 
+            this.lblDireccion.AutoSize = true;
+            this.lblDireccion.Location = new System.Drawing.Point(11, 45);
+            this.lblDireccion.Name = "lblDireccion";
+            this.lblDireccion.Size = new System.Drawing.Size(52, 13);
+            this.lblDireccion.TabIndex = 4;
+            this.lblDireccion.Text = "Dirección";
+            // 
+            // lblGEO
+            // 
+            this.lblGEO.AutoSize = true;
+            this.lblGEO.Location = new System.Drawing.Point(11, 93);
+            this.lblGEO.Name = "lblGEO";
+            this.lblGEO.Size = new System.Drawing.Size(30, 13);
+            this.lblGEO.TabIndex = 5;
+            this.lblGEO.Text = "GEO";
+            // 
+            // lblLocalidad
+            // 
+            this.lblLocalidad.AutoSize = true;
+            this.lblLocalidad.Location = new System.Drawing.Point(11, 141);
+            this.lblLocalidad.Name = "lblLocalidad";
+            this.lblLocalidad.Size = new System.Drawing.Size(53, 13);
+            this.lblLocalidad.TabIndex = 6;
+            this.lblLocalidad.Text = "Localidad";
+            // 
+            // lblProvincia
+            // 
+            this.lblProvincia.AutoSize = true;
+            this.lblProvincia.Location = new System.Drawing.Point(11, 186);
+            this.lblProvincia.Name = "lblProvincia";
+            this.lblProvincia.Size = new System.Drawing.Size(51, 13);
+            this.lblProvincia.TabIndex = 7;
+            this.lblProvincia.Text = "Provincia";
+            // 
+            // txtDNI
+            // 
+            this.txtDNI.Location = new System.Drawing.Point(138, 38);
+            this.txtDNI.Name = "txtDNI";
+            this.txtDNI.Size = new System.Drawing.Size(100, 20);
+            this.txtDNI.TabIndex = 8;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(138, 89);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtApellido.TabIndex = 9;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(138, 138);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 10;
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(138, 38);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(100, 20);
+            this.txtDireccion.TabIndex = 11;
+            // 
+            // txtGEO
+            // 
+            this.txtGEO.Location = new System.Drawing.Point(138, 86);
+            this.txtGEO.Name = "txtGEO";
+            this.txtGEO.Size = new System.Drawing.Size(100, 20);
+            this.txtGEO.TabIndex = 12;
+            // 
+            // cmbLocalidad
+            // 
+            this.cmbLocalidad.FormattingEnabled = true;
+            this.cmbLocalidad.Location = new System.Drawing.Point(138, 133);
+            this.cmbLocalidad.Name = "cmbLocalidad";
+            this.cmbLocalidad.Size = new System.Drawing.Size(100, 21);
+            this.cmbLocalidad.TabIndex = 13;
+            // 
+            // cmbProvincia
+            // 
+            this.cmbProvincia.FormattingEnabled = true;
+            this.cmbProvincia.Location = new System.Drawing.Point(138, 178);
+            this.cmbProvincia.Name = "cmbProvincia";
+            this.cmbProvincia.Size = new System.Drawing.Size(100, 21);
+            this.cmbProvincia.TabIndex = 14;
+            // 
+            // frmRRHH
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gbDomicilio);
+            this.Controls.Add(this.gbDatos);
+            this.Name = "frmRRHH";
             this.Text = "frmRRHH";
+            this.gbDatos.ResumeLayout(false);
+            this.gbDatos.PerformLayout();
+            this.gbDomicilio.ResumeLayout(false);
+            this.gbDomicilio.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox gbDatos;
+        private System.Windows.Forms.GroupBox gbDomicilio;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtDNI;
+        private System.Windows.Forms.Label lblProvincia;
+        private System.Windows.Forms.Label lblLocalidad;
+        private System.Windows.Forms.Label lblGEO;
+        private System.Windows.Forms.Label lblDireccion;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.Label lblDNI;
+        private System.Windows.Forms.ComboBox cmbProvincia;
+        private System.Windows.Forms.ComboBox cmbLocalidad;
+        private System.Windows.Forms.TextBox txtGEO;
     }
 }
